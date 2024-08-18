038fc30764dedcad7292de8dab8a0c304ea89c15
marksikaundi <emmanuelmark117@gmail.com>
2024-08-18 23:47:47 +0200

    updates

---
+++ src/components/forms/sign-up/form-provider.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const SignUpFormProvider = ({ children }: Props) => {
+    return <div className="">
+
+    </div>;  
+}
+
+
+export default SignUpFormProvider;
---
+++ src/context/use-auth-context.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+type InitialValuesProps = {
+    currentStep: number;
+    setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const InitialValues : InitialValuesProps = {
+    currentStep: 1,
+    setCurrentStep: () => undefined,
+}
+
